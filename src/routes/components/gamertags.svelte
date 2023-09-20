29f9c5b573d0429e00fc6042254cc138e3d39df2
Hao Joe <fortytwoyeah@42YEAHS-RAZER.lan>
2023-09-20 18:21:34 +0800

    More gamertags

--- src/routes/components/gamertags.svelte
+++ src/routes/components/gamertags.svelte
@@ -7,13 +7,19 @@
     <GamerTag name="SEM" prof="熟练" />
     <GamerTag name="TEM" prof="熟练" />
     <GamerTag name="XPS" prof="熟练" />
+    <GamerTag name="Raman" prof="熟练" />
+    <GamerTag name="FT-IR" prof="熟练" />
+    <GamerTag name="BET" prof="熟练" />
+    <GamerTag name="EDS" prof="熟练" />
+    <GamerTag name="ICP" prof="熟练" />
     <GamerTag name="CV" prof="熟练" />
     <GamerTag name="EIS" prof="熟练" />
+    <GamerTag name="GITT" prof="熟练" />
+    <GamerTag name="LSV" prof="熟练" />
     <GamerTag name="Origin" prof="熟练" />
     <GamerTag name="ChemDraw" prof="熟练" />
     <GamerTag name="Blender" prof="熟练" />
-    <GamerTag name="Raman" prof="熟练" />
-    <GamerTag name="FT-IR" prof="熟练" />
+    <GamerTag name="CAD" prof="熟练" />
 </div>
 
 <style>
@@ -22,6 +28,5 @@
         row-gap: 0.1cm;
         flex-wrap: wrap;
         column-gap: 0.1cm;
-        justify-content: space-between;
     }
 </style>
